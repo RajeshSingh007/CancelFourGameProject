5dbf1672244bf656ab4de29261ec8e4b007116bc
rajesh singh <rajesh@rajeshs-MacBook-Pro.local>
2020-09-27 17:02:55 +0530

    Updates

---
+++ precache-manifest.39d317d2393cb94c4ed221e49e7b0d80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7df477d098521f8ee1d345ff6297d59",
+    "url": "/CancelFourGameProject/index.html"
+  },
+  {
+    "revision": "97beb5ef708237b46a57",
+    "url": "/CancelFourGameProject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/CancelFourGameProject/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CancelFourGameProject/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97beb5ef708237b46a57",
+    "url": "/CancelFourGameProject/static/js/main.fa351353.chunk.js"
+  },
+  {
+    "revision": "3b6a732d1d14c4b700cd",
+    "url": "/CancelFourGameProject/static/js/runtime-main.a5334f6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CancelFourGameProject/static/media/logo.5d5d9eef.svg"
+  }
+]);
